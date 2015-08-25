8850309d8ad1850ba6567b61442ce37075c60ad8
James Skimming <JSkimming@users.noreply.github.com>
2015-08-25 15:55:24 +0100

    Started adding tests for the HttpClient requests

---
+++ src/Tests/CloudFlare.NET.Tests/HttpHandlers/TestHttpHandler.cs
@@ -0,0 +1,109 @@
+﻿namespace CloudFlare.NET.HttpHandlers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Net;
+    using System.Net.Http;
+    using System.Text;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Newtonsoft.Json;
+    using Newtonsoft.Json.Linq;
+
+    /// <summary>
+    /// ServiceFilter that allows a test to control the HTTP pipeline and
+    /// analyze a request and provide a set response.
+    /// </summary>
+    public class TestHttpHandler : DelegatingHandler
+    {
+        readonly HttpResponseMessage _nullResponse;
+        int _responseIndex;
+
+        public TestHttpHandler()
+        {
+            Requests = new List<HttpRequestMessage>();
+            Responses = new List<HttpResponseMessage>();
+            RequestContents = new List<string>();
+
+            _nullResponse = CreateResponse(string.Empty);
+        }
+
+        public HttpRequestMessage Request
+        {
+            get { return Requests.Count == 0 ? null : Requests[Requests.Count - 1]; }
+            set
+            {
+                Requests.Clear();
+                Requests.Add(value);
+            }
+        }
+
+        public List<HttpRequestMessage> Requests { get; set; }
+        public List<string> RequestContents { get; set; }
+
+        public HttpResponseMessage Response
+        {
+            get { return Responses.Count == 0 ? null : Responses[Responses.Count - 1]; }
+            set
+            {
+                _responseIndex = 0;
+                Responses.Clear();
+                Responses.Add(value);
+            }
+        }
+
+        public List<HttpResponseMessage> Responses { get; set; }
+
+        public Func<HttpRequestMessage, Task<HttpRequestMessage>> OnSendingRequest { get; set; }
+
+        public void SetResponseContent(object content)
+        {
+            SetResponseContent(JObject.FromObject(content).ToString(Formatting.None));
+        }
+
+        public void SetResponseContent(string content)
+        {
+            Response = CreateResponse(content);
+        }
+
+        public void AddResponseContent(string content)
+        {
+            Responses.Add(CreateResponse(content));
+        }
+
+        protected async override Task<HttpResponseMessage> SendAsync(
+            HttpRequestMessage request,
+            CancellationToken cancellationToken)
+        {
+            string content = request.Content == null
+                ? null
+                : await request.Content.ReadAsStringAsync().ConfigureAwait(false);
+            RequestContents.Add(content);
+
+            if (OnSendingRequest != null)
+            {
+                Requests.Add(await OnSendingRequest(request).ConfigureAwait(false));
+            }
+            else
+            {
+                Requests.Add(request);
+            }
+
+            if (_responseIndex < Responses.Count)
+            {
+                return Responses[_responseIndex++];
+            }
+
+            return _nullResponse;
+        }
+
+        public static HttpResponseMessage CreateResponse(string content, HttpStatusCode code = HttpStatusCode.OK)
+        {
+            return new HttpResponseMessage(code)
+            {
+                Content = new StringContent(content, Encoding.UTF8, "application/json")
+            };
+        }
+
+    }
+}
---
+++ src/Tests/CloudFlare.NET.Tests/ZoneClientSpec.cs
@@ -0,0 +1,35 @@
+﻿namespace CloudFlare.NET.ZoneSpec
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Net.Http;
+    using Machine.Specifications;
+    using Ploeh.AutoFixture;
+
+    [Subject(typeof(CloudFlareClient))]
+    public class When_getting_all_zones : RequestContext
+    {
+        static IReadOnlyList<Zone> _expected;
+        static IReadOnlyList<Zone> _actual;
+
+        Establish context = () =>
+        {
+            var response = _fixture.Create<CloudFlareResponse<IReadOnlyList<Zone>>>();
+            _expected = response.Result;
+            _handler.SetResponseContent(response);
+        };
+
+        Because of = () => _actual = _sut.GetZonesAsync(_auth).Await().AsTask.Result;
+
+        Behaves_like<AuthenticatedRequestBehaviour> authenticated_request_behaviour;
+
+        It should_make_a_GET_request = () => _handler.Request.Method.ShouldEqual(HttpMethod.Get);
+
+        It should_request_the_zones_endpoint = () =>
+            _handler.Request.RequestUri.ShouldEqual(new Uri(CloudFlareConstants.BaseUri, "zones"));
+
+        It should_return_the_expected_zones = () =>
+            _actual.Select(z => z.AsLikeness().CreateProxy()).SequenceEqual(_expected).ShouldBeTrue();
+    }
+}
