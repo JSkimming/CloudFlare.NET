64db32de0dc140e7262ee2bc088ca6b5dad46a48
James Skimming <JSkimming@users.noreply.github.com>
2015-08-25 17:15:37 +0100

    Tested the get DNS records request

--- src/Tests/CloudFlare.NET.Tests/DnsRecordClientExtensionsSpec.cs
+++ src/Tests/CloudFlare.NET.Tests/DnsRecordClientExtensionsSpec.cs
@@ -3,13 +3,10 @@
     using System;
     using System.Collections.Generic;
     using System.Linq;
-    using System.Text;
     using System.Threading;
-    using System.Threading.Tasks;
     using Machine.Specifications;
     using Moq;
     using Ploeh.AutoFixture;
-    using MoqIt = Moq.It;
     using It = Machine.Specifications.It;
 
     [Subject(typeof(DnsRecordClientExtensions))]
