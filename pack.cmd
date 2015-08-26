b58e08a4cf7f85a81efe4d19315f7a68c7050cc1
James Skimming <JSkimming@users.noreply.github.com>
2015-08-26 12:28:19 +0100

    Create packages for CloudFlare.NET.Yaml

--- pack.cmd
+++ pack.cmd
@@ -7,3 +7,6 @@
 
 @echo "%~dp0src\.nuget\NuGet.exe" pack "%~dp0src\CloudFlare.NET\CloudFlare.NET.csproj" -Properties Configuration=%config% -NonInteractive -Symbols -OutputDirectory "%~dp0src\CloudFlare.NET\bin\%config%"
 @"%~dp0src\.nuget\NuGet.exe" pack "%~dp0src\CloudFlare.NET\CloudFlare.NET.csproj" -Properties Configuration=%config% -NonInteractive -Symbols -OutputDirectory "%~dp0src\CloudFlare.NET\bin\%config%"
+
+@echo "%~dp0src\.nuget\NuGet.exe" pack "%~dp0src\CloudFlare.NET.Yaml\CloudFlare.NET.Yaml.csproj" -Properties Configuration=%config% -NonInteractive -Symbols -OutputDirectory "%~dp0src\CloudFlare.NET.Yaml\bin\%config%"
+@"%~dp0src\.nuget\NuGet.exe" pack "%~dp0src\CloudFlare.NET.Yaml\CloudFlare.NET.Yaml.csproj" -Properties Configuration=%config% -NonInteractive -Symbols -OutputDirectory "%~dp0src\CloudFlare.NET.Yaml\bin\%config%"
