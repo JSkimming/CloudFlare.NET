7b1627065d3c08e634fc287fbbade8bdad529cc7
James Skimming <JSkimming@users.noreply.github.com>
2015-08-27 12:49:50 +0100

    Added conversion from parameters to a query string

--- src/Tests/CloudFlare.NET.Tests/Serialization/BehaviorTemplates.cs
+++ src/Tests/CloudFlare.NET.Tests/Serialization/BehaviorTemplates.cs
@@ -72,4 +72,22 @@
 
         It should_serialize_match = () => _sut.Match.ToString().ShouldEqual(_json["match"].Value<string>());
     }
+
+    [Behaviors]
+    public class PagedParametersKvpBehavior<TOrder>
+        where TOrder : struct
+    {
+        protected static PagedParameters<TOrder> _parameters;
+        protected static IReadOnlyDictionary<string, string> _result;
+
+        It should_have_name_page = () => _result["page"].ShouldEqual(_parameters.Page.ToString());
+
+        It should_have_name_per_page = () => _result["per_page"].ShouldEqual(_parameters.PerPage.ToString());
+
+        It should_have_name_order = () => _result["order"].ShouldEqual(_parameters.Order.ToString());
+
+        It should_have_name_direction = () => _result["direction"].ShouldEqual(_parameters.Direction.ToString());
+
+        It should_have_name_match = () => _result["match"].ShouldEqual(_parameters.Match.ToString());
+    }
 }
--- src/Tests/CloudFlare.NET.Tests/Serialization/PagedZoneParametersSerializationSpec.cs
+++ src/Tests/CloudFlare.NET.Tests/Serialization/PagedZoneParametersSerializationSpec.cs
@@ -2,7 +2,9 @@
 {
     using System;
     using System.Collections.Generic;
+    using System.Collections.Specialized;
     using System.Linq;
+    using System.Net.Http;
     using Machine.Specifications;
     using Newtonsoft.Json;
     using Newtonsoft.Json.Linq;
@@ -69,5 +71,77 @@
                 .WithDefaultEquality(e => e.Match)
                 .WithDefaultEquality(e => e.PerPage)
                 .ShouldEqual(_expected);
+    }
+
+    [Subject(typeof (PagedZoneParameters))]
+    public class When_converting_to_key_value_pair_with_all_default_values
+    {
+        static PagedZoneParameters _parameters;
+        static IEnumerable<KeyValuePair<string, string>> _result;
+
+        Establish context = () => _parameters = new PagedZoneParameters();
+
+        Because of = () => _result = _parameters.ToKvp();
+
+        It should_have_no_values = () => _result.ShouldBeEmpty();
+    }
+
+    [Subject(typeof(PagedZoneParameters))]
+    public class When_converting_to_key_value_pair_with_no_default_values : FixtureContext
+    {
+        protected static PagedZoneParameters _parameters;
+        protected static Dictionary<string, string> _result;
+
+        Establish context = () =>
+        {
+            // Auto fixture chooses the default value for enumerations.
+            _fixture.Inject(PagedZoneOrderFieldTypes.email);
+            _fixture.Inject(PagedParametersOrderType.desc);
+            _fixture.Inject(PagedParametersMatchType.any);
+
+            _parameters = _fixture.Create<PagedZoneParameters>();
+        };
+
+        Because of = () => _result = _parameters.ToKvp().ToDictionary(kvp => kvp.Key, kvp => kvp.Value);
+
+        Behaves_like<PagedParametersKvpBehavior<PagedZoneOrderFieldTypes>> paged_parameters_kvp_behavior;
+
+        It should_have_name_value = () => _result["name"].ShouldEqual(_parameters.Name);
+
+        It should_have_name_status = () => _result["status"].ShouldEqual(_parameters.Status.ToString());
+    }
+
+    [Subject(typeof(PagedZoneParameters))]
+    public class When_converting_to_query_string : FixtureContext
+    {
+        protected static PagedZoneParameters _parameters;
+        protected static Dictionary<string, string> _result;
+
+        Establish context = () =>
+        {
+            // Auto fixture chooses the default value for enumerations.
+            _fixture.Inject(PagedZoneOrderFieldTypes.email);
+            _fixture.Inject(PagedParametersOrderType.desc);
+            _fixture.Inject(PagedParametersMatchType.any);
+
+            _parameters = _fixture.Create<PagedZoneParameters>();
+        };
+
+        Because of = () =>
+        {
+            string query = _parameters.ToQuery();
+            // Convert
+            var builder = new UriBuilder("http://localhost/path") { Query = query };
+
+            // Get the values back as a dictionary to validate their values.
+            NameValueCollection kvp = builder.Uri.ParseQueryString();
+            _result = kvp.Cast<string>().ToDictionary(k => k, k => kvp[k]);
+        };
+
+        Behaves_like<PagedParametersKvpBehavior<PagedZoneOrderFieldTypes>> paged_parameters_kvp_behavior;
+
+        It should_have_name_value = () => _result["name"].ShouldEqual(_parameters.Name);
+
+        It should_have_name_status = () => _result["status"].ShouldEqual(_parameters.Status.ToString());
     }
 }
